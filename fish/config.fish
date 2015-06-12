f164aefafc4f9a748bad2f76eb7725f9f46b7675
Maxim Starostenko <starosten.co@gmail.com>
2015-06-12 15:27:04 +0600

    F-0 add fish config

---
+++ fish/config.fish
@@ -0,0 +1,64 @@
+# Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
+
+# Theme
+set fish_theme bobthefish
+
+# All built-in plugins can be found at ~/.oh-my-fish/plugins/
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Enable plugins by adding their name separated by a space to the line below.
+set fish_plugins theme
+
+# Path to your custom folder (default path is ~/.oh-my-fish/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
+
+
+function ll
+	ls --human-readable -l
+end
+
+function myman
+	less ~/.myman.txt
+end
+
+# Хоткеи для навигации
+function ..
+	cd ..
+end
+
+function ...
+	cd ../..
+end
+
+function ....
+	cd ../../..
+end
+
+function .....
+	cd ../../../..
+end
+
+function ......
+	cd ../../../../..
+end
+
+# Создать директорию и перейти в неё же. С параметром -p
+function mkdircd -d "Create a directory and set CWD"
+        command mkdir -p $argv
+        if test $status = 0
+                switch $argv[(count $argv)]
+                        case '-*'
+                        case '*'
+                                cd $argv[(count $argv)]
+                                return
+                end
+        end
+end
+
+alias "c=xclip"
+alias "v=xclip -o"
+
+set -g theme_display_ruby no
